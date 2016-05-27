e12619c931ff9666637ba8420af0ec57b3c86f4b
Sony Zacharia <sony.z@bridgeconn.com>
2016-05-27 18:01:00 +0530

    modifications

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  win.loadURL(`file:${__dirname}/app/nav.html`);
+  win.loadURL(`file:${__dirname}/app/views/index.html`);
 
   // Open the DevTools.
   win.webContents.openDevTools();
