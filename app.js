66a8162c7f7ee9751db050a8d5a940d4b532123a
Joel Mathew <joelmathew.mec@gmail.com>
2016-05-31 20:34:11 -0700

    Changing paths.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  win.loadURL(`file:${__dirname}/app/views/index.html`);
+  win.loadURL(`file:${__dirname}/assets/index.html`);
 
   // Open the DevTools.
   win.webContents.openDevTools();
