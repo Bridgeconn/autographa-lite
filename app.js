e44290fec2396dbdab15142695d5532d21abbecd
Joel Mathew <joelmathew.mec@gmail.com>
2016-05-31 23:28:43 -0700

    Adding photon.

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    'min-width': 600,
+    'min-height': 300,
+    'accept-first-mouse': true,
+    'title-bar-style': 'hidden'
+  });
 
   // and load the index.html of the app.
   win.loadURL(`file:${__dirname}/assets/index.html`);
