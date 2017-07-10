a7a2b48c481fe439f18f586671d44aa598979d73
Joel Mathew <joelmathew.mec@gmail.com>
2017-07-09 21:47:05 -0700

    Fix close issue in Mac

--- app.js
+++ app.js
@@ -60,12 +60,7 @@ function createWindow() {
           else{
             win = null;
           }
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      if (process.platform !== 'darwin') {
-          app.quit();
-      }
+      app.quit();
     });
 }
 
